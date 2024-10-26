3d7e72a3c8c023e6a8758372ba89642b2f8cb2b9
Darren Danvers <darren@darrenhacks.io>
2024-10-26 18:05:54 -0500

    update the domain of the container images

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 ## Builds and tags the container
-docker build . -t dev.darrencodes/docker-c:1.0
+docker build . -t io.darrenhacks/docker-c:1.0
 
 ## Runs the image and removes the container once complete.
-docker run --rm dev.darrencodes/docker-c:1.0
+docker run --rm io.darrenhacks/docker-c:1.0
 
